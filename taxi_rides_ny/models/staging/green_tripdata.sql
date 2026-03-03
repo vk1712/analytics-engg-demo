90fce209345868d0115180066d3542f49ef92cfe
Vishal Kumar <33510648+vk1712@users.noreply.github.com>
2026-03-03 10:19:38 +0000

    commit

--- taxi_rides_ny/models/staging/green_tripdata.sql
+++
@@ -1,2 +0,0 @@
--- select * from taxi_rides_ny.prod.green_tripdata
-select * from {{ source('raw_data', 'green_tripdata') }}
---
+++ taxi_rides_ny/models/staging/stg_green_tripdata.sql
@@ -0,0 +1,30 @@
+-- select * from taxi_rides_ny.prod.green_tripdata
+select
+    -- identifiers
+    cast(vendorid as int) as vendor_id,
+    ratecodeid as rate_code_id,
+    pulocationid as pickup_location_id,
+    dolocationid as dropoff_location_id,
+
+    -- timestamps
+    cast(lpep_pickup_datetime as timestamp) as pickup_datetime,
+    cast(lpep_dropoff_datetime as timestamp) as dropoff_datetime,
+
+    -- trip info
+    store_and_fwd_flag,
+    cast(passenger_count as int) as passenger_count,
+    cast(trip_distance as float) as trip_distance,
+    trip_type,
+    
+    -- payment info
+    cast(fare_amount as numeric) as fare_amount,
+    cast(extra as numeric) as extra,
+    cast(mta_tax as numeric) as mta_tax,
+    cast(tip_amount as numeric) as tip_amount,
+    cast(tolls_amount as numeric) as tolls_amount,
+    cast(improvement_surcharge as numeric) as improvement_surcharge,
+    cast(total_amount as numeric) as total_amount,
+    cast(payment_type as int) as payment_type
+
+from {{ source('raw_data', 'green_tripdata') }}
+where vendorid is not null

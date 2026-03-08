bea6628e60302c169adae86674174f1dfd70f266
Vishal Kumar <33510648+vk1712@users.noreply.github.com>
2026-03-08 07:45:59 +0000

    commit

--- taxi_rides_ny/models/marts/fct_trips.sql
+++ taxi_rides_ny/models/marts/fct_trips.sql
@@ -1,1 +1,14 @@
-select 1
+with trips_unioned AS (
+    select * from {{ ref('int_trips_unioned') }}
+),
+payment_type_name AS (
+    select * from {{ ref('payment_type_lookup') }}
+)
+select
+    *,
+    concat(tu.vendor_id, '~', tu.pickup_datetime, '~', tu.dropoff_datetime, '~', tu.pickup_location_id, '~', tu.dropoff_location_id) as trip_id
+from 
+    trips_unioned tu
+        join
+    payment_type_name ptn on tu.payment_type = ptn.payment_type
+where tu.payment_type != 3;
